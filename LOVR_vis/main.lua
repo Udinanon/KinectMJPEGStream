e26fe1f5e04b3b48037edd0a30c92f2750fcd3ee
Udinanon <m.trapanotto@tutamail.com>
2025-05-15 17:54:24 +0200

    Small example of consuming data from the server via LOVR, and storing in a texture

---
+++ LOVR_vis/main.lua
@@ -0,0 +1,25 @@
+local http = require 'http'
+function lovr.load()
+
+
+    local status, data, headers = http.request("0.0.0.0:8080/rgb_once")
+    --local parsed_data = json.decode(data)
+    local image = lovr.data.newImage(lovr.data.newBlob(data))
+    texture = lovr.graphics.newTexture(image, {mipmaps = false, usage = {"sample", "transfer"}})
+end
+
+function lovr.draw(pass)
+    pass:setColor(1, 0, 0)
+    pass:line(0, 0, 0, 1, 0, 0)
+    pass:setColor(0, 1, 0)
+    pass:line(0, 0, 0, 0, 1, 0)
+    pass:setColor(0, 0, 1)
+    pass:line(0, 0, 0, 0, 0, 1)
+    pass:setColor(1, 1, 1, 1)
+    local status, data, headers = http.request("0.0.0.0:8080/depth_once")
+    --local parsed_data = json.decode(data)
+    local image = lovr.data.newImage(lovr.data.newBlob(data))
+    texture:setPixels(image)
+    pass:setMaterial(texture)
+    pass:cube()
+end
