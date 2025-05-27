0a62c4682e989982c9eb0358bfb17f9461e9bd64
Udinanon <m.trapanotto@tutamail.com>
2025-05-27 17:20:05 +0200

    Using the libfreenect_sycn lib we can access the kiect and display data, now just the frame timestamp

--- server/hello_browser.c
+++ server/hello_browser.c
@@ -1,8 +1,9 @@
+#include <libfreenect/libfreenect_sync.h>
 #include <microhttpd.h>
+#include <png.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #define PORT 8888
 
@@ -11,8 +12,7 @@ enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *connectio
                                      const char *method, const char *version,
                                      const char *upload_data,
                                      long unsigned int *upload_data_size, void **con_cls) {
-  const char *page = "<html><body>Hello, browser!</body></html>";
-
+  
   int n;
   unsigned long fileLen;
   FILE *fptr;
@@ -44,13 +44,21 @@ enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *connectio
   char str[1024];  // just big enough
 
   snprintf(str, sizeof str, "%u", fileLen);
-  
+
+  char *data;
+  unsigned int timestamp;
+  freenect_sync_get_video((void **)(&data), &timestamp, 0, FREENECT_VIDEO_RGB);
+  char* page;
+  asprintf(&page, "<html><body>Hello, browser! Timestamp= %u </body></html>", timestamp);
+
   struct MHD_Response *response;
   int ret;
-
-  response = MHD_create_response_from_buffer(fileLen,
-                                             (void *)buffer, MHD_RESPMEM_PERSISTENT);
-  MHD_add_response_header(response, "Content-Type", "image/jpeg");
+  response = MHD_create_response_from_buffer(strlen(page),
+                                             (void *)page, MHD_RESPMEM_PERSISTENT);
+  //MHD_add_response_header(response, "Content-Type", "image/png");
+  //response = MHD_create_response_from_buffer(640*480*3,
+  //                                           (void *)data, MHD_RESPMEM_PERSISTENT);
+  //MHD_add_response_header(response, "Content-Type", "image/png");
   //MHD_add_response_header(response, "Content-Length", (const char *)str);
   ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
   MHD_destroy_response(response);
@@ -59,7 +67,9 @@ enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *connectio
 }
 
 int main() {
+
   struct MHD_Daemon *daemon;
+
 
   daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
                             &answer_to_connection, NULL, MHD_OPTION_END);
